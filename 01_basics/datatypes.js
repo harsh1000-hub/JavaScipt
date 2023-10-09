5607cec2ab72c8a450e6aee3208d1245aa76a026
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2023-10-09 17:00:44 +0530

    tech 1

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+// Video 5:-
+
+"use strict"; // basically represent yur code in current version of js
+
+// alert(3+3)  // this will work in browswer but we are run our js file in node js enviroment
+
+// datatypes in js
+// 1. number 2. string 3. boolean 4. symbol 5. null = standalone value 6. undefined 7. bigInt
+let accountName = "Rahul"
+let age = 22
+let check = true
+let accountState;
+let city = null
+console.log(typeof "Harsh")  // typeof = tell which type of datatype your value hold
+console.log(typeof undefined);   // o/p -> undefined
+console.table([accountName,age,check,accountState,city])
---
+++ 01_basics/variables.js
@@ -0,0 +1,28 @@
+// Chai aur code :-
+// Video 4
+const accountId = 12345  // const type variable value fixed you are not able to modify in later program
+var accountMail = "harsh@google.com"  
+let accountPassword = "212121"
+accountCity = "Kanpur"
+
+// accountId = 83939  -> This line give error  
+// console.log(accountId);
+
+// difference b/w var and let is 
+accountMail = "hvd@yahoo.com"
+accountPassword = "10022"
+accountCity = "Bengaluru"
+console.table([accountId,accountMail,accountPassword,accountCity])
+/* console.table print the variable value in table format
+┌─────────┬─────────────────┐
+│ (index) │     Values      │
+├─────────┼─────────────────┤
+│    0    │      12345      │
+│    1    │ 'hvd@yahoo.com' │
+│    2    │     '10022'     │
+│    3    │   'Bengaluru'   │
+└─────────┴─────────────────┘
+*/ // Complete Video 4
+
+
+
