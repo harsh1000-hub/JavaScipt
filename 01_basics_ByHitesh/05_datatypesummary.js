1a124e8fc544bb0e9bf37f7fe2898c32c90c1181
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2023-11-30 14:14:40 +0530

    Tech1

--- 01_basics_ByHitesh/05_datatypesummary.js
+++ 01_basics_ByHitesh/05_datatypesummary.js
@@ -25,7 +25,7 @@ console.log(typeof examId);
 
 // Array
 const cricketer = ['Virat','Rohit','Gill']
-console.log(typeof cricketer);
+console.log(typeof cricketer); // O/P -> objects
 console.log(cricketer);
 
 const myObj= {
@@ -39,5 +39,33 @@ console.log(myObj);
 const myFunction = function(){
     console.log("Hello World");
 }
-console.log(typeof myFunction);
+console.log(typeof myFunction);  // O/P -> Function 
+// But technically in JS typeof myFunction = object function
 console.log(myFunction);
+
+
+// ------------------------------------------------------------------------------------
+
+// Video 10:-
+// Stack VS Heap Memory
+// Stack = Store all Primitive datatype and make a copy 
+// Heap = Store all Non-Primitive datatype/Referrence datatype and use referrence concept
+
+// Stack example
+let studentName = "Harsh";
+let anotherStudentName = studentName;  // In this only copy of a studentName assign to anotherStudnetName not original referrence
+anotherStudentName = "Vaibhav"; // No effect on studentName due to stack memory
+console.log(studentName);
+console.log(anotherStudentName);
+
+// Heap example
+const employeeDetails = {
+    name:"Harsh",
+    age:"22",
+    email:"harsh@google.com"
+}
+
+const anotherEmployeeDetails = employeeDetails;
+anotherEmployeeDetails.email="vaibhav@google.com" // this line make change in anotherEmployeeDetails as well employeeDetaisl due to assign referrence
+console.table([employeeDetails]);
+console.table([anotherEmployeeDetails]);
